f22bb61168d8c5e2825aa8cc8e0543b98efff00a
Varun T <varunvarun60954@gmail.com>
2025-04-03 18:53:45 +0530

    user authentication api completed

---
+++ backend/controllers/userController.js
@@ -0,0 +1,72 @@
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res, next) => {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.send({
+        success: false,
+        message: "user already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    req.body.password = hashedPassword;
+
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    res.send({
+      message: "user created successfully",
+      data: null,
+      success: true,
+    });
+  } catch (error) {
+    res.send({ message: error.message, success: false });
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.send({
+        success: false,
+        message: "user does not exists",
+      });
+    }
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      return res.send({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    if (!user.isVerified) {
+      return res.send({
+        success: false,
+        message: "User is not yet Verified or has been suspended",
+      });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.send({
+      message: "user logged in successfully",
+      data: token,
+      success: true,
+    });
+  } catch (error) {
+    res.send({ message: error.message, success: false });
+  }
+};
+
+
--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,15 @@ dotenv.config();
 
 connectDB(); 
 
+const userRoute = require("./routes/userRoute");
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
+app.use("/api/users", userRoute)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
