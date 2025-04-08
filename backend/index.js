e3a6d99b0337f7fd88e7e0aec41b8366f02e6a3d
Varun T <varunvarun60954@gmail.com>
2025-04-08 10:30:18 +0530

    set up for deployment

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const path = require("path") 
+
 
 dotenv.config();
 
@@ -17,6 +19,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api/users", userRoute);
 app.use("/api/transactions", transactionRoute);
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
